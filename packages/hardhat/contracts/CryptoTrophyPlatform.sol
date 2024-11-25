997d49ef8ce1b7ac23ae1d58e426570dcbcbdbc6
Francisco Viñas <fvinas@fi.uba.ar>
2024-11-25 00:37:52 -0300

    change: comment validator logic

--- packages/hardhat/contracts/CryptoTrophyPlatform.sol
+++ packages/hardhat/contracts/CryptoTrophyPlatform.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 // Ejemplo del contrato de tokens
 contract CompanyToken is ERC20, Ownable {
 	constructor(
@@ -16,14 +15,14 @@ contract CompanyToken is ERC20, Ownable {
 		_mint(msg.sender, initialSupply * (10 ** decimals()));
 	}
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
+	function mint(address to, uint256 amount) public onlyOwner {
+		_mint(to, amount);
+	}
 }
 
-interface IValidator {
-	function validate(address user) external view returns (bool);
-}
+// interface IValidator {
+// 	function validate(address user) external view returns (bool);
+// }
 
 contract CryptoTrophyPlatform {
 	// Estructuras
@@ -33,16 +32,16 @@ contract CryptoTrophyPlatform {
 		address token;
 		mapping(address => bool) adminExists;
 		mapping(address => bool) userExists;
-        address[] admins;
-        address[] users;
+		address[] admins;
+		address[] users;
 		uint256[] challengeIds;
-        bool exists;
+		bool exists;
 	}
 
 	struct Challenge {
 		uint256 id;
 		string description;
-		address validator;
+		// address validator;
 		uint256 prizeAmount;
 		uint256 startTime;
 		uint256 endTime;
@@ -51,7 +50,7 @@ contract CryptoTrophyPlatform {
 		uint256 winnerCount;
 		uint256 orgId;
 		mapping(address => bool) winners;
-        bool exists;
+		bool exists;
 	}
 
 	// Atributos
@@ -90,7 +89,7 @@ contract CryptoTrophyPlatform {
 		string memory _symbol,
 		uint256 _initialSupply
 	) {
-        // Crear token de la plataforma
+		// Crear token de la plataforma
 		cryptoTrophyToken = address(
 			new CompanyToken(_name, _symbol, _initialSupply)
 		);
@@ -120,60 +119,52 @@ contract CryptoTrophyPlatform {
 		org.id = orgId;
 		org.name = _name;
 		org.token = token;
-        org.exists = true;
+		org.exists = true;
 
-        // Por default, el creador de la organización es admin
-        _addAdmin(orgId, msg.sender);
+		// Por default, el creador de la organización es admin
+		_addAdmin(orgId, msg.sender);
 
 		for (uint256 i = 0; i < _admins.length; i++) {
 			_addAdmin(orgId, _admins[i]);
 		}
 		for (uint256 i = 0; i < _users.length; i++) {
 			_addUser(orgId, _users[i]);
-        }
+		}
 
 		organizationIds.push(orgId);
 		emit OrganizationCreated(orgId, _name, token);
 		return orgId;
 	}
 
-	/// @notice Obtiene información básica de una organización
-	function getOrganization(
-		uint256 _orgId
-	) public view returns (uint256, string memory, address) {
-		Organization storage org = organizations[_orgId];
-		return (org.id, org.name, org.token);
+	function _addAdmin(uint256 _orgId, address _admin) internal {
+		if (!organizations[_orgId].adminExists[_admin]) {
+			organizations[_orgId].adminExists[_admin] = true;
+			organizations[_orgId].admins.push(_admin);
+		}
 	}
-
-    function _addAdmin(uint256 _orgId, address _admin) internal {
-        if (!organizations[_orgId].adminExists[_admin]) {
-            organizations[_orgId].adminExists[_admin] = true;
-            organizations[_orgId].admins.push(_admin);
-        }
-    }
 
 	/// @notice Agrega un administrador a una organización
 	function addAdmin(uint256 _orgId, address _admin) public onlyAdmin(_orgId) {
-        _addAdmin(_orgId, _admin);
+		_addAdmin(_orgId, _admin);
 	}
 
-    function _addUser(uint256 _orgId, address _user) internal {
-        if (!organizations[_orgId].userExists[_user]) {
-            organizations[_orgId].userExists[_user] = true;
-            organizations[_orgId].users.push(_user);
-        }
-    }
+	function _addUser(uint256 _orgId, address _user) internal {
+		if (!organizations[_orgId].userExists[_user]) {
+			organizations[_orgId].userExists[_user] = true;
+			organizations[_orgId].users.push(_user);
+		}
+	}
 
 	/// @notice Agrega un usuario a una organización
 	function addUser(uint256 _orgId, address _user) public onlyAdmin(_orgId) {
-        _addUser(_orgId, _user);
+		_addUser(_orgId, _user);
 	}
 
 	/// @notice Crea un nuevo desafío
 	function createChallenge(
 		uint256 _orgId,
 		string memory _description,
-		address _validator,
+		// address _validator,
 		uint256 _prizeAmount,
 		uint256 _startTime,
 		uint256 _endTime,
@@ -185,17 +176,17 @@ contract CryptoTrophyPlatform {
 		Challenge storage challenge = challenges[challengeId];
 		challenge.id = challengeId;
 		challenge.description = _description;
-		challenge.validator = _validator;
+		// challenge.validator = _validator;
 		challenge.prizeAmount = _prizeAmount;
 		challenge.startTime = _startTime;
 		challenge.endTime = _endTime;
 		challenge.maxWinners = _maxWinners;
 		challenge.orgId = _orgId;
 		challenge.active = true;
-        challenge.exists = true;
+		challenge.exists = true;
 
-        Organization storage organization = organizations[_orgId];
-        require(organization.exists, "Organization does not exist");
+		Organization storage organization = organizations[_orgId];
+		require(organization.exists, "Organization does not exist");
 
 		organization.challengeIds.push(challengeId);
 		challengeIds.push(challengeId);
@@ -221,8 +212,8 @@ contract CryptoTrophyPlatform {
 		);
 		require(!challenge.winners[msg.sender], "Already claimed");
 
-		IValidator validator = IValidator(challenge.validator);
-		require(validator.validate(msg.sender), "Validation failed");
+		// IValidator validator = IValidator(challenge.validator);
+		// require(validator.validate(msg.sender), "Validation failed");
 
 		Organization storage organization = organizations[_orgId];
 
@@ -240,6 +231,30 @@ contract CryptoTrophyPlatform {
 	function listOrganizations() public view returns (uint256[] memory) {
 		return organizationIds;
 	}
+
+	// /// @notice Lista todas las organizaciones donde el sender es admin
+	// function listOrganizationsAsAdmin() public view returns (uint256[] memory) {
+	// 	uint256[] memory orgs = new uint256[](orgCount);
+	// 	uint256 count = 0;
+	// 	for (uint256 i = 0; i < orgCount; i++) {
+	// 		if (organizations[i].adminExists[msg.sender]) {
+	// 			orgs[count++] = i;
+	// 		}
+	// 	}
+	// 	return orgs;
+	// }
+
+	// /// @notice Lista todas las organizaciones donde el sender es usuario
+	// function listOrganizationsAsUser() public view returns (uint256[] memory) {
+	// 	uint256[] memory orgs = new uint256[](orgCount);
+	// 	uint256 count = 0;
+	// 	for (uint256 i = 0; i < orgCount; i++) {
+	// 		if (organizations[i].userExists[msg.sender]) {
+	// 			orgs[count++] = i;
+	// 		}
+	// 	}
+	// 	return orgs;
+	// }
 
 	/// @notice Lista todos los desafíos de una organización
 	function listChallenges(
